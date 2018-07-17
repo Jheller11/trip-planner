67935e1580df5ddb7db5cdcc424fc5908c9142a7
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-07-17 13:13:00 -0400

    trip views/controller

--- controllers/trips.js
+++ controllers/trips.js
@@ -2,8 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Trip = require('../models/Trip')
 
+router.get('/show', (req, res) => {
+  res.render('trips/show')
+})
+
+router.get('/new', (req, res) => {
+  res.render('trips/new')
+})
+
+router.get('/edit', (req, res) => {
+  res.render('trips/edit')
+})
+
 router.get('/', (req, res) => {
-  res.send('trip controller')
+  res.render('trips/index')
 })
 
 module.exports = router
